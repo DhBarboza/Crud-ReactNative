c14329647f8916bc8ab959ce12d26ae58dc216de
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-05-11 21:53:27 -0300

    Init config page from list view

--- crud-app/screens/ItemsList.js
+++ crud-app/screens/ItemsList.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import { Text, View } from 'react-native';
+import firebase from '../database/firebase';
 
 
 const ItemsList = () => {
+
+    const [items, setState] = useState([])
+
+    useEffect(() => {
+        firebase.db.collection('anotations').onSnapshot(querySnapshot => {
+            querySnapshot.docs.forEach(doc => {
+                console.log(doc.data)
+            })
+        })
+    }, [])
+
     return (
         <View>
             <Text>Lista de Items</Text>
